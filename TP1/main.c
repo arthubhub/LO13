7308cd19948959e4cc4521b5b22eb3fe4fd0f28e
McArthur <mcarthur@wifi-etudiants091.utt.fr>
2025-03-20 14:36:19 +0100

    First square

--- TP1/main.c
+++ TP1/main.c
@@ -1,7 +1,36 @@
 #include <stdlib.h>
-#include "GUI.h"
+#include <GL/glut.h>
+
+void display(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.5,-0.5);
+        glVertex2f(0.5,-0.5);
+        glVertex2f(0.5,0.5);
+        glVertex2f(-0.5,0.5);
+    glEnd();
+    glFlush();
+}
+
+void init(){
+    glClearColor(0.0,0.0,0.0,0.0);
+    glColor3f(1.0,1.0,1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1.0,1.0,-1.0,1.0);
+}
+
 
 int main(int argc, char **argv)
 {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("LO13");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+
     return EXIT_SUCCESS;
 }
